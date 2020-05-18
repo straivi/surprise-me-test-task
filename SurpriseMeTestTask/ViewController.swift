f969ee633c266715a4bfdad7838b6aff1aae41d3
matvey <bme15122000@gmail.com>
2020-05-18 23:25:29 +0500

    Initial Commit

---
+++ SurpriseMeTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurpriseMeTestTask
+//
+//  Created by  Matvey on 18.05.2020.
+//  Copyright © 2020 Borisov Matvei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
