5dea2551a5160d89fbc5e802227d678132aff126
matvey <bme15122000@gmail.com>
2020-05-23 18:14:11 +0500

    add counry picker lib

---
+++ Pods/Target Support Files/Pods-SurpriseMeTestTask/Pods-SurpriseMeTestTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SurpriseMeTestTask : NSObject
+@end
+@implementation PodsDummy_Pods_SurpriseMeTestTask
+@end
--- SurpriseMeTestTask/PresentationLayer/CountryCode/CountryCodeViewController.swift
+++ SurpriseMeTestTask/PresentationLayer/CountryCode/CountryCodeViewController.swift
@@ -6,4 +6,8 @@
 //  Copyright © 2020 Borisov Matvei. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CountryCodeViewController: UIViewController {
+  
+}
